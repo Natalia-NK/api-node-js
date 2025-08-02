b2c0ee71062a71f2eb5d9266a24c84b6f432cce8
Asty <asty@itdevels.eu>
2025-08-02 20:51:52 +0300

    add empty test

---
+++ tests/user-controller.test.ts
@@ -0,0 +1,7 @@
+// tests/api.spec.ts
+import { test, expect } from '@playwright/test';
+let baseURL: string = 'http://localhost:3000/users';
+
+test('Dummy test', () => {
+    expect(true).toBe(true);
+})
